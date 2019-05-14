9c791f602019544e75b219caca6cb30f104d91b3
Case Duckworth <acdw@acdw.net>
2019-05-13 21:18:42 -0500

    Add YAML list of members

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+from yaml import safe_load
 
 nethoop = Flask(__name__)
 
 @nethoop.route('/')
 def root():
-    return "Nethoop v. 0.0.1"
+    ret = "Nethoop v. 0.0.1"
+          "Members: "+hoop_members('data/members.yaml')
+    return ret
+
+def hoop_members(fname):
+    return yaml.safe_load(fname)
 
 if __name__ == '__main__':
     nethoop.run()
