4cda46b25c6770f110762d4b261765fe977f8fbb
Case Duckworth <acdw@acdw.net>
2019-05-13 22:37:27 -0500

    Add title and correct mimetype

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, render_template
 from yaml import safe_load
 
 app = Flask(__name__)
 
+TITLE = "Writers of the Fediverse!"
+
 
 @app.route("/")
 def root():
-    return render_template("index.html", members=hoop_members())
+    return render_template("index.html", title=TITLE, members=hoop_members())
 
 
 @app.route("/feeds.xml")
-def feeds():
-    return render_template("feeds.xml", members=hoop_members())
+def feeds_xml():
+    response = make_response(
+        render_template("feeds.xml", title=TITLE, members=hoop_members())
+    )
+    response.headers["Content-Type"] = "application/xml"
+    return response
 
 
 def hoop_members():
